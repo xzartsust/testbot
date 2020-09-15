00aa2c55eb4739adb7eab515393313168cbcb322
xzartsust <68950796+xzartsust@users.noreply.github.com>
2020-09-15 18:57:21 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import os
+
+bot = commands.Bot(command_prefix = '/')
+
+@bot.event
+async def on_ready():
+    print(f'Bot {bot.user.name} is connected')
+
+@bot.command()
+async def ping(ctx):
+    user = ctx.message.author
+    await ctx.send(f'Pong {user.mention}')
+
+TOKEN = os.environ.get('TOKEN')
+
+
+bot.run(TOKEN)
