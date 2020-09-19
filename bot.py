3438ee3bbe5d3ed8ccd2b03f43afaba7df47fe4b
xzartsust <68950796+xzartsust@users.noreply.github.com>
2020-09-19 11:21:50 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,16 +4,25 @@ import os
 
 bot = commands.Bot(command_prefix = '/')
 
+words = ['bb', 'пока', 'пп']
+
 @bot.event
 async def on_ready():
     print(f'Bot {bot.user.name} is connected')
 
 @bot.command()
-async def ping(ctx, text: str):
+async def ping(ctx, *, text: str):
     user = ctx.message.author
     await ctx.send(f'Pong {user.mention} {text}')
 
-TOKEN = os.environ.get('TOKEN')
+@bot.event
+async def on_message(message):
 
+    await bot.process_commands(message)
+    user = ctx.message.author
+    if message in words:
+        await ctx.send(f'Hello {user.mention}')
+
+TOKEN = os.environ.get('TOKEN')
 
 bot.run(TOKEN)
