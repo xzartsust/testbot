7ac7cec52b6abbd5b33277d7f95c0dcbe811aeee
xzartsust <68950796+xzartsust@users.noreply.github.com>
2020-09-19 11:07:39 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -9,9 +9,9 @@ async def on_ready():
     print(f'Bot {bot.user.name} is connected')
 
 @bot.command()
-async def ping(ctx, *, text: str):
+async def ping(ctx, *, text: str, text1: str):
     user = ctx.message.author
-    await ctx.send(f'Pong {user.mention} {text}')
+    await ctx.send(f'Pong {user.mention} {text} {text1}')
 
 TOKEN = os.environ.get('TOKEN')
 
