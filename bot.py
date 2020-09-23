4b9eb1d17b6c01d6e79464b83e8a1f32be47f47f
xzartsust <68950796+xzartsust@users.noreply.github.com>
2020-09-23 16:50:09 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,9 +4,25 @@ import os
 
 bot = commands.Bot(command_prefix = '/')
 
+hello_words = ['Hi', 'Hello', 'Привіт']
+
+
 @bot.event
 async def on_ready():
     print(f'Bot {bot.user.name} is connected')
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+
+    user = ctx.message.author
+
+    msg = message.content.lower()
+
+    if mgs in hello_words:
+        await ctx.send(f'Привіт {user.mention}')
+    
+
 
 @bot.command()
 async def ping(ctx, *, text: str):
